f38f80b967a364a61859eb059f25a5171033d46f
Alex Aswin Rajappa <alexaswinrajappa@Alexs-MacBook-Air.local>
2025-08-12 18:34:00 -0400

    Initial commit

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,69 @@
+import { KeyValue, NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import * as Papa from 'papaparse';
+import { CommonModule } from '@angular/common';
+
+interface MenuItem {
+  
+  Category: string;
+  ItemName: string;
+  Price: number;
+  Availability: string;
+}
+
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [HttpClientModule, NgFor, CommonModule ],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent  implements OnInit {
+
+  menuItems: any = [];
+  groupedMenuItem: { [category: string]: MenuItem[] } = {};
+  originalOrder = (a: KeyValue<string, any>, b: KeyValue<string, any>): number => {
+    return 0;  // No sorting, preserve original order
+  }
+  
+
+  private menuItemsURL  = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTUlfPw6Rxh8AfTPDEeDjB18OADT3Shz2dZJtHQwcg1xE2ioYiYSN-t9t41eDB7XvXsV3vaKppw-7K-/pub?gid=0&single=true&output=csv';
+
+  constructor( private http: HttpClient) { }
+  
+  ngOnInit(): void {
+  this.getMenuItems();
+  }
+
+  getMenuItems() {
+    this.http.get(this.menuItemsURL, { responseType: 'text' }).subscribe(data => {
+      const parsed = Papa.parse(data, { header: true });
+      this.menuItems = parsed.data;
+  
+      this.groupedMenuItem = this.menuItems.reduce((groups: { [key: string]: any[] }, item: any) => {
+        
+        // Use 'Category' as per your CSV header name
+        const category = (item.Category ? String(item.Category) : 'Uncategorized').trim();
+        if (!groups[category]) {
+          groups[category] = [];
+        }
+        groups[category].push(item);
+        return groups;
+      }, {});
+  
+      console.log(this.groupedMenuItem);
+    });  
+
+    const originalOrder = (a: KeyValue<string, any>, b: KeyValue<string, any>): number => {
+      return 0;  // No sorting, preserve original order
+    }
+  }
+
+  
+
+
+
+}
